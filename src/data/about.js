7bbeef21fd0480bfe48bdcbb7596f3bf87869d73
thesashidiggikar <20210801042@dypiu.ac.in>
2025-07-13 15:22:03 +0530

    Updated

--- src/data/about.js
+++ src/data/about.js
@@ -1,11 +1,11 @@
 const aboutData = {
-  name: "Shreyash Maurya",
-  headline: "Aspiring Software Developer | MCA (AI & Data Science) | BCA (Cloud Computing)",
-  summary: `Aspiring Software Developer pursuing an MCA in Artificial Intelligence and Data Science, with a BCA degree (Specialization in Cloud Computing) and top academic honors. Strong expertise in cross-platform application development, cloud computing (AWS, Firebase), and AI-driven solutions. Passionate about designing efficient, user-centric software while staying updated with the latest industry trends and technologies. Seeking to contribute technical expertise and innovation to a dynamic team.`,
+  name: "Sakshi Diggikar",
+  headline: "Aspiring Gen AI Engineer | MCA (AI & Data Science) | BCA (Cloud Computing)",
+  summary: `Aspiring Generative AI Engineer pursuing an MCA in Artificial Intelligence and Data Science, with a BCA degree (Specialization in Cloud Computing) and top academic honors. Strong expertise in cross-platform application development, cloud computing (AWS, Firebase), and AI-driven solutions. Passionate about designing efficient, user-centric software while staying updated with the latest industry trends and technologies. Seeking to contribute technical expertise and innovation to a dynamic team.`,
   contact: {
-    email: "shreyashmaurya3112@gmail.com",
-    phone: "+91 7666336947",
-    linkedin: "https://www.linkedin.com/in/the-shreyashmaurya"
+    email: "sakshidiggikar681@gmail.com",
+    phone: "+91 8788593897",
+    linkedin: "https://www.linkedin.com/in/sakshi-diggikar-a02b6b1b2/"
   },
   location: "Pune, India"
 };
